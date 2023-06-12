0d2863eae36f5ee707de1fdde348c4650a51ac6d
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-12 17:52:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [meal, setMeal] = useState([]);
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((res) => res.json())
+      .then((data) => {
+        setMeal(data);
+        console.log(data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1>{meal.categories.length}</h1>
+      </div>
     </div>
   );
 }
