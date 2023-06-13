4213a6bbe627af725ac411fe7db5dfbb27733be4
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-13 10:31:16 +0600

    contact us page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -35,6 +35,29 @@ const useStyles = createStyles((theme) => ({
     color: theme.colors[theme.primaryColor][0],
     fontSize: theme.fontSizes.sm,
     marginTop: rem(5),
+    width: rem(250),
+  },
+  heading: {
+    fontFamily: `Inter, sans-serif ${theme.fontFamily}`,
+    fontWeight: 700,
+    color: "#151515",
+    lineHeight: 1.2,
+    fontSize: rem(32),
+    // marginTop: theme.spacing.xs,
+    textTransform: "uppercase",
+    textAlign: "center",
+    // paddingBottom: theme.spacing.sm,
+  },
+  subTitle: {
+    fontFamily: `Inter, sans-serif ${theme.fontFamily}`,
+    fontWeight: 500,
+    color: "#D99904",
+    lineHeight: 1.2,
+    fontSize: rem(12),
+    marginTop: theme.spacing.xs,
+    textTransform: "uppercase",
+    textAlign: "center",
+    paddingBottom: theme.spacing.xs,
   },
 
   stat: {
@@ -66,7 +89,7 @@ const data = [
   {
     title: "Address",
     description:
-      " 123 ABS Street, Uni 21, Bangladesh  +88 123456789  Mon - Fri: 08:00 - 22:00,Sat - Sun: 10:00 - 23:00",
+      "123 ABS Street, Uni 21, Bangladesh  +88 123456789  Mon - Fri: 08:00 - 22:00,Sat - Sun: 10:00 - 23:00",
   },
   {
     title: "Working Hours",
@@ -77,15 +100,23 @@ const ContactUs = () => {
   const { classes } = useStyles();
   const stats = data.map((stat) => (
     <div key={stat.title} className={classes.stat}>
-      <Text className={classes.count}>{stat.stats}</Text>
+      {/* <Text className={classes.count}>{stat.stats}</Text> */}
       <Text className={classes.title}>{stat.title}</Text>
       <Text className={classes.description}>{stat.description}</Text>
     </div>
   ));
   return (
-    <div className="py-28">
-      <Container className={classes.root}>{stats}</Container>
-    </div>
+    <Container className="py-28">
+      <div>
+        <div className={classes.heading} color="#151515">
+          <h1>Contact Us</h1>
+        </div>
+        <div className={classes.subTitle}>
+          <p>Visit Us</p>
+        </div>
+      </div>
+      <div className={classes.root}>{stats}</div>
+    </Container>
   );
 };
 
