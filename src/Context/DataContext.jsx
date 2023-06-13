319c4ed9a6e94756a4f7a3b93fb30b52bb9db588
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-13 09:58:10 +0600

    testimonial section

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -1,11 +1,25 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ApiContext = createContext();
 
 const DataContext = ({ children }) => {
+  const [foodItems, setFoodItems] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setFoodItems(data);
+      });
+  }, []);
+
+  const foodInfo = {
+    foodItems,
+  };
+
   return (
     <div>
-      <ApiContext.Provider>{children}</ApiContext.Provider>
+      <ApiContext.Provider value={foodInfo}>{children}</ApiContext.Provider>
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
+import Header from "../Components/Header";
+import OrderOnline from "../Components/OrderOnline";
+import { Testimonials } from "../Components/Testimonials";
 
 const Home = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("data.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenu(data);
-        console.log(data);
-      });
-  }, []);
+  // const [menu, setMenu] = useState([]);
+  // useEffect(() => {
+  //   fetch("data.json")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenu(data);
+  //       console.log(data);
+  //     });
+  // }, []);
   return (
     <div>
-      <h1>
+      {/* <h1>
+      <a href="https://ibb.co/qBF2Fk4"><img src="https://i.ibb.co/Zd1b1Ww/header-img.png" alt="header-img" border="0"></a>
         {menu.map((item) => (
           <div>{item.category}</div>
         ))}
-      </h1>
+      </h1> */}
+
+      <Header></Header>
+      <OrderOnline></OrderOnline>
+      <Testimonials></Testimonials>
     </div>
   );
 };
