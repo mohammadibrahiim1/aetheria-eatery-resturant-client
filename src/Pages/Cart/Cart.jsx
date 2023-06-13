9a00545d92b81fb96b4c9ebf33d9a78a929f320b
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-06-14 01:29:26 +0600

    updated

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -6,12 +6,9 @@ import {
   Group,
   Container,
   Grid,
-  AspectRatio,
 } from "@mantine/core";
 import { useContext, useState } from "react";
 import { ApiContext } from "../../Context/DataContext";
-// import CartItem from "../../Components/CartItem";
-// import { others } from "@chakra-ui/react";
 
 const useStyles = createStyles((theme) => ({
   card: {
@@ -54,26 +51,6 @@ const Cart = () => {
     }, 0);
   };
 
-  // let total = 0;
-  // let quantity = 0;
-  // for (const item of cart) {
-  //   quantity = quantity + item.quantity;
-  //   total = total + item.price * item.quantity;
-  // }
-
-  // const grandTotal = total;
-  // const [productQuantity, setProductQuantity] = useState(1);
-  // const [foodPrice, setFoodPrice] = useState(null);
-
-  // const increaseQuantity = (id, item) => {
-  //   setProductQuantity(item.quantity + 1);
-  // };
-  // const decreaseQuantity = (id, item) => {
-  //   setProductQuantity(item.quantity - 1);
-  // };
-
-  //   const cards = mockdata.map((article) => (
-
   return (
     <div>
       <Container className="">
@@ -93,14 +70,6 @@ const Cart = () => {
                     <Group noWrap spacing={0} key={product.id}>
                       <Image src={product.image} height={140} width={140} />
                       <div className={classes.body}>
-                        {/* <Text
-                          transform="uppercase"
-                          color="dimmed"
-                          weight={700}
-                          size="xs"
-                        >
-                          {item.category}
-                        </Text> */}
                         <Text className={classes.title} mt="xs" mb="md">
                           {product.name}
                         </Text>
@@ -108,15 +77,6 @@ const Cart = () => {
                           ${product.price * product.quantity}
                         </Text>
                         <div class="flex justify-between items-center   border-gray-100">
-                          {/* <div className="mr-5"> */}
-                          {/* <span
-                            onClick={() => decreaseQuantity(item.id)}
-                            class="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"
-                          >
-                            {" "}
-                            -{" "}
-                          </span> */}
-
                           <input
                             onChange={(e) =>
                               handleQuantityChange(
@@ -127,31 +87,12 @@ const Cart = () => {
                             class="h-8 w-8 border bg-white text-center text-xs outline-none"
                             type="number"
                             value={product.quantity}
-                            // min="1"
                           />
-
-                          {/* <p className="pb-5">
-                            Subtotal: ${product.price * product.quantity}
-                          </p> */}
-                          {/* <span
-                            onClick={() => increaseQuantity(item.id)}
-                            class="cursor-pointer rounded-r bg-gray-100 py-1 px-3 mr-5 duration-100 hover:bg-blue-500 hover:text-blue-50"
-                          >
-                            {" "}
-                            +{" "}
-                          </span> */}
-                          {/* </div> */}
-                          {/* <span onClick={() => handleRemoveProduct(id)}>
-                            <FaTrash className="text-error cursor-pointer" />
-                          </span> */}
                         </div>
-                        <Group noWrap spacing="xs">
-      
-                        </Group>
+                        <Group noWrap spacing="xs"></Group>
                       </div>
                     </Group>
                   </Card>
-                  {/* <CartItem item={item}></CartItem> */}
                 </>
               ))}
             </div>
@@ -187,17 +128,6 @@ const Cart = () => {
           </Grid.Col>
         </Grid>
       </Container>
-      {/* <Container>
-        {" "}
-        <Grid>
-          <Grid.Col md={6} lg={3}>
-            cat product
-          </Grid.Col>
-          <Grid.Col md={6} lg={3}>
-            calculate cart item price
-          </Grid.Col>
-        </Grid>
-      </Container> */}
     </div>
   );
 };
