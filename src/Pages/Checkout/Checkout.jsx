de76cde320da485874a1af7f261f118119becb48
mohammadibrahiim1 <mohammadibrahiim1@gmail.com>
2023-07-04 22:04:28 +0600

    update cart style

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -9,21 +9,21 @@ const useStyles = createStyles((theme) => ({
   section: {
     width: "60%",
     margin: "auto",
-    // paddingTop: "50px",
-    // paddingBottom: "50px",
+    paddingTop: "50px",
+    paddingBottom: "50px",
     // border: "1px solid gray",
     // borderRadius: "15px",
   },
   payment_method: {
     border: "1px solid gray",
     borderRadius: "15px",
-    // padding: "20px",
+    padding: "20px",
   },
   paymentCard: {
     border: "1px solid gray",
     borderRadius: "15px",
     // width: '90%'
-    // padding: "20px",
+    padding: "20px",
   },
   heading: {
     fontWeight: 700,
@@ -46,32 +46,21 @@ const Checkout = () => {
   const { classes } = useStyles();
   return (
     <div>
-      <section className={classes.section}>
-        <Grid className="py-8 ">
-          <Grid.Col sm={12} xs={12} lg={8} className={classes.payment_method}>
-            <p
-            // class={classes.heading}
-            >
-              Select payment method
-            </p>
+      <Grid className={classes.section}>
+        <Grid.Col sm={12} xs={12} lg={8}>
+          <div className={classes.payment_method}>
+            <p class={classes.heading}>Select payment method</p>
             <div class="mt-4">
               <PaymentSavedCart></PaymentSavedCart>
             </div>
 
-            <p
-            //  class={classes.paymentCard_heading}
-            >
-              New payment method
-            </p>
-          </Grid.Col>
-          <Grid.Col sm={12} xs={12} lg={1}></Grid.Col>
-          <Grid.Col sm={12} xs={12} lg={3} class={classes.paymentCard}>
-            {/* <div> */}
-            <p
-            // class={classes.heading}
-            >
-              Payment Details
-            </p>
+            <p class={classes.paymentCard_heading}>New payment method</p>
+          </div>
+        </Grid.Col>
+        <Grid.Col sm={12} xs={12} lg={0.5}></Grid.Col>
+        <Grid.Col sm={12} xs={12} lg={3.5}>
+          <div class={classes.paymentCard}>
+            <p class={classes.heading}>Payment Details</p>
             <p class="">
               Complete your order by providing your payment details.
             </p>
@@ -90,9 +79,9 @@ const Checkout = () => {
                 <PaymentCard></PaymentCard>
               </div>
             </div>
-          </Grid.Col>
-        </Grid>
-      </section>
+          </div>
+        </Grid.Col>
+      </Grid>
     </div>
   );
 };
